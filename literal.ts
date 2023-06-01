be326bc543eb413e8416421545c4482af1753753
Truong-Nghia <truongnghia25298@gmail.com>
2023-06-01 23:00:06 +0700

    literal

---
+++ literal.ts
@@ -0,0 +1,12 @@
+let changingString = "Hello World";
+changingString = "Olá Mundo";
+changingString;
+
+const constantString = "Hello World";
+constantString;
+
+let greetings: "hello" = "hello";
+// OK
+greetings = "hello";
+// ...
+greetings = "xin chào";
